4cfa0347a98e4d3353dac2f68ac84bee17887a22
Akif9748 <70021050+Akif9748@users.noreply.github.com>
2022-01-13 00:36:20 +0300

    Added download.js info
    New file

--- index.js
+++ index.js
@@ -2,41 +2,12 @@
 const first = 116,
   last = 116,
   path = `./images/`,
-  link = "https://www.silicone-forum.com"
+  link = "https://www.silicone-forum.com";
 
-const fs = require("fs");
-const axios = require("axios");
-
+const download = require("./download.js");
 
 for (var i = first; i <= last; i++) {
-  download(i)
+  download(i);
 }
 
 
-
-function download(i) {
-
-  if (i > 999) {
-    const number = i.toString()
-    var url = `${link}/data/avatars/o/${number.substring(0, number.length - 3)}/${number}.jpg`
-  } else {
-    var url = `${link}/data/avatars/o/0/${i}.jpg`
-  }
-
-  axios.get(url, {
-    responseType: 'arraybuffer'
-  })
-    .then((response)=> {
-      // handle success
-      const buffer = Buffer.from(response.data, 'base64');
-      fs.writeFileSync(`${path + i}.jpg`, buffer)
-      console.log(`${i} is success.`, response.status);
-
-
-    })
-    .catch((error)=>  {
-      // handle error
-      return console.error(`${i} is not success.`, "Cant found image.");
-
-    })
-}
