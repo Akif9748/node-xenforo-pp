a72c57c34aab8d509a357afd1472d71fd4367edc
Akif9748 <70021050+Akif9748@users.noreply.github.com>
2022-01-13 00:37:48 +0300

    Extra file

---
+++ download.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const axios = require("axios");
+module.exports = (i) => {
+
+  if (i > 999) {
+    const number = i.toString()
+    var url = `${link}/data/avatars/o/${number.substring(0, number.length - 3)}/${number}.jpg`
+  } else {
+    var url = `${link}/data/avatars/o/0/${i}.jpg`
+  }
+
+  axios.get(url, {
+    responseType: 'arraybuffer'
+  })
+    .then((response)=> {
+      // handle success
+      const buffer = Buffer.from(response.data, 'base64');
+      fs.writeFileSync(`${path + i}.jpg`, buffer)
+      console.log(`${i} is success.`, response.status);
+
+
+    })
+    .catch((error)=>  {
+      // handle error
+      return console.error(`${i} is not success.`, "Cant found image.");
+
+    })
+}
