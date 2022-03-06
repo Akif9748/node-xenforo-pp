66982fdbcc86dd04291066b79d2bad12fdca54e1
Akif Yüce <70021050+Akif9748@users.noreply.github.com>
2022-03-06 16:23:12 +0300

    Version 1.1

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # node-xenforo-pp
-You can download profile photos in Xenforo based forums
+You can download profile photos in range in Xenforo based forums, less bloat, and fastest way for this work.
+
+## Usage:
+Configure the config in file:
+```js
+//Config:
+const first = 1, 
+last = 50, 
+//Download
+path = "./images/",
+//Forum URL:
+link = "https://www.silicone-forum.com";
+```
+- Write `npm i` to terminal for install dependencies.
+- Write `node index`, and download is started.
+
+## Image at working:
+
+
--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-//Minimum value is 1 for first.
-const first = 116,
-  last = 116,
-  path = `./images/`,
-  link = "https://www.silicone-forum.com";
+const fs = require("fs");
+const fetch = require("node-fetch");
 
-const download = require("./download.js");
+//Config:
+const first = 1, last = 50, path = "./images/", link = "https://www.silicone-forum.com";
 
-for (var i = first; i <= last; i++) {
-  download(i);
+let success = 0;
+
+async function download(i) {
+
+  const response = await fetch(
+    i >= 1000 ?
+      `${link}/data/avatars/o/${(i / 1000).toString().substring(0, number.length - 3)}/${number}.jpg`
+      : `${link}/data/avatars/o/0/${i}.jpg`
+
+  );
+
+  if (response.status === 200) {
+    success++;
+    console.log(i, "is success.");
+    return fs.writeFileSync(`${path + i}.jpg`, await response.buffer());
+
+  } else
+    return console.error(i, "is not success.");
+
 }
 
+async function main() {
+  console.time("Finished in");
 
+  for (i = first; i <= last; i++)
+    await download(i);
+
+  console.timeEnd("Finished in");
+  console.log("Success image count:", success);
+}
+
+main();
